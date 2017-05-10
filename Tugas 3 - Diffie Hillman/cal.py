c724b68abf8d24ef5d85b8056e0e8d770ca77870
Mustika Kurnia M <nia.kurniam@yahoo.co.id>
2017-05-10 09:47:32 +0700

    update tugas 3

--- Tugas 3 - Diffie Hillman/cal.py
+++ Tugas 3 - Diffie Hillman/cal.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 a=11000        #angka persetujuan kurang dari bilangan prima
 b=100153        #angka persetujuan bilangan prima
 
@@ -21,28 +20,4 @@ print y1
 #print >> sys.stderr, 'B:"%s"' % data
                 #print >> sys.stderr, 'sending data back to the client'
 
-=======
-a=11000        #angka persetujuan kurang dari bilangan prima
-b=100153        #angka persetujuan bilangan prima
-
-c=1000        #random number A
-f=2017       #random number b
-
-
-x=(a**c)%b
-print x
-
-y=(a**f)%b
-print y
-
-x1=(y**c)%b
-print x1
-
-y1=(x**f)%b
-print y1
-
-#print >> sys.stderr, 'B:"%s"' % data
-                #print >> sys.stderr, 'sending data back to the client'
-
->>>>>>> 67d74d60e69b37020b3f98357e1666c2df1f152a
                 #print >> sys.stderr, 'no more data from', client_address
